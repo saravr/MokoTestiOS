8d453359905aad73f69ad119efd927e4e75c70e1
Sarav Ramaswamy <sarav@yahooinc.com>
2022-12-29 11:37:14 -0800

    Initial Commit

---
+++ MokoTestiOS/MokoTestiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MokoTestiOSApp.swift
+//  MokoTestiOS
+//
+//  Created by Sarav Ramaswamy on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct MokoTestiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
